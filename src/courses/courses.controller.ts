39b4de145e7bd118275f4348bcc9d2d102aa4b38
eliasfernandes <eliasfernandes@CPX-N23ZMCQ8C6B.dir.svc.accenture.com>
2022-03-16 15:13:50 -0300

    feature: create methods Patch and Delete to controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,17 +1,22 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
 
     @Get()
-    findAll() {
-        return 'Listagem de cursos'
+    findAll(@Res() response) {
+        return response.status(200).send({
+            curso1: "JS",
+            curso2: "JAVA",
+            curso3: "C#"
+        });
     }
 
     @Get(':id')
     findParams(@Param() params) { //BUSCAMAIS DE UM PARAMETRO
         return `Curso #${params.id}`
     }
+
     @Get(':idd')
     findParam(@Param('idd') idd: string) { //BUSCA UM UNICO PARAMETRO
         return `Curso #${idd}`
@@ -19,9 +24,19 @@ export class CoursesController {
 
     @Post()
     @HttpCode(201)
-    create(@Body('name') body) {
+    create(@Body() body) {
         return body
 
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body('name') body) {
+        return `Course Update #${id}`
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) { //DELETE
+        return `Exclusão do Curso #${id}`
     }
 }
 
