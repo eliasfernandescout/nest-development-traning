75f88c2e180bff983a21076a808d4bdcdc59f0da
elias.fernandes.neto <elias.fernandes.neto@accenture.com>
2022-03-21 06:46:05 -0300

    feat: create the http methos on controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -8,38 +8,28 @@ export class CoursesController {
 
     @Get()
     findAll(@Res() response) {
-        return response.status(200).send({
-            curso1: "JS",
-            curso2: "JAVA",
-            curso3: "C#"
-        });
+        return this.coursesService.findAll();
     }
 
     @Get(':id')
-    findParams(@Param() params) { //BUSCAMAIS DE UM PARAMETRO
-        return `Curso #${params.id}`
-    }
-
-    @Get(':idd')
-    findParam(@Param('idd') idd: string) { //BUSCA UM UNICO PARAMETRO
-        return `Curso #${idd}`
+    findOne(@Param('íd') id: string) {
+        return this.coursesService.findOne(id);
     }
 
     @Post()
-    @HttpCode(201)
     create(@Body() body) {
-        return body
+        return this.coursesService.create(body)
 
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body('name') body) {
-        return `Course Update #${id}`
+    update(@Param('id') id: string, @Body() body) {
+        return this.coursesService.update(id, body)
     }
 
     @Delete(':id')
     remove(@Param('id') id: string) { //DELETE
-        return `Exclusão do Curso #${id}`
+        return this.coursesService.delete(id)
     }
 }
 
