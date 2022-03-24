61d207bfd1b1e2d2200ae7800f4d1755cd0e78f9
eliasfernandescout <eliasfernandescout@gmail.com>
2022-03-24 16:26:30 -0300

    refactor: add PartialType for UpdateCourseDto

--- src/courses/dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto.ts
@@ -1,12 +1,7 @@
-import { IsString } from "class-validator";
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateCourseDto } from "./create-course.dto";
 
-export class UpdateCourseDto {
-    @IsString()
-    readonly name?: string;
-    @IsString()
-    readonly description?: string;
-    @IsString({ each: true })
-    readonly tags?: string[];
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {
 }
 
 
