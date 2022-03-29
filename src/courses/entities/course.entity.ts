d939bcb2c322ee02b6350aed84c1be545031fdb4
eliasfrnandes <eliasfernandescout@gmail.com>
2022-03-28 21:16:39 -0300

    feat: create typeorm entity

--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,6 +1,16 @@
-export class Course{
-    id: number;
-    name: string;
-    description: string;
-    tags: string[];
-}                                   
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity('courses')
+export class CourseEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @Column()
+    description: string
+
+    @Column('json', { nullable: true })
+    tags: string[]
+}
